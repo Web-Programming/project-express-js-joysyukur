25c63fff893768345c784641a2af690e66bceb7e
joysyukur <joysyukurtatemaberkatdohona_2327240112@mhs.mdp.ac.id>
2025-09-29 17:00:51 +0700

    pertemuan6

--- toko-online/app_toko_online/routes/index.js
+++ toko-online/app_toko_online/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var products = require('../data/products.json');
+var products = require('../../data/products.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
