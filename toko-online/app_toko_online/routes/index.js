faeac5e987e14a129cbafe9e932eb8dc9cb37c76
joysyukur <joysyukurtatemaberkatdohona_2327240112@mhs.mdp.ac.id>
2025-10-06 15:35:33 +0700

    latihan

--- toko-online/app_toko_online/routes/index.js
+++ toko-online/app_toko_online/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var products = require('../../data/products.json');
+var mainController = require('../../controller/main');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Toko Onlinde Sederhana',
-     products: products
-     });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Toko Onlinde Sederhana',
+//      products: products
+//      });
+// });
+  router.get("/", mainController.index);
 
 module.exports = router;
